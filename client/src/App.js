214807f9287f83d10ab3774503fce5989495ad5b
Giovanna Cunha <giovannakelli@gmail.com>
2021-05-24 21:25:46 -0300

    setup apollo/client and react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
-import logo from './logo.svg';
+import { ApolloProvider } from '@apollo/client';
+import { client } from './ApolloProvider';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+import { Home } from './pages/Home';
+import { Login } from './pages/Login';
+import { Register } from './pages/Register';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <ApolloProvider client={client}>
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/register" component={Register}/>
+      </Router>
+    </ApolloProvider>
   );
 }
 
